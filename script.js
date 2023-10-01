1c3e40f18ea71d2b66606566633fa843bb92a4a5
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-01 16:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const agent = navigator.userAgent;
 const broName = navigator.appName;
 const ver = navigator.appVersion;
 const ans = `You are using ${broName} version ${ver}`
+
+const container = document.getElementsByClassName("browser-info")[0];
+container.innerText = ans;
