a4e087261aac21b1a88117a0a6284ac0d534a98e
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-01 16:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const agent = navigator.userAgent;
+const broName = navigator.appName;
+const ver = navigator.appVersion;
+const ans = `You are using ${broName} version ${ver}`
